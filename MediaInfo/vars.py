9b3cf04afc825754b13b8f520a9bed129cbfe309
DeekshithSH <shdeekshith@gmail.com>
2024-04-28 22:15:34 +0530

    Default port to 8080 and fixed wrong requirement

--- MediaInfo/vars.py
+++ MediaInfo/vars.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 load_dotenv(".env")
 
 class Var(object):
-    PORT=int(environ.get("PORT", None))
+    PORT=int(environ.get("PORT", 8080))
     DEBUG=str(environ.get("DEBUG", "0").lower()) in ("1", "true", "t", "yes", "y")
     BIND_ADDRESS="0.0.0.0"
     PROJECT_PATH = pathlib.Path(__file__).parent

6fe40f15ae92d401009599a17201264c8a9c2fba
DeekshithSH <shdeekshith@gmail.com>
2024-04-28 21:42:38 +0530

    Get URL Support

--- MediaInfo/routes.py
+++ MediaInfo/routes.py
@@ -1,6 +1,8 @@
 import os
 
-from aiohttp import web
+from aiohttp import web, ClientSession, ClientTimeout
+from aiohttp.client_exceptions import InvalidURL
+from asyncio.exceptions import TimeoutError
 from bson import ObjectId
 
 from MediaInfo.utils.file import get_metadata
@@ -8,13 +10,13 @@ from MediaInfo.vars import Var
 
 routes = web.RouteTableDef()
 
-@routes.post("/api")
+@routes.post("/api/file")
 async def root_route_handler(request: web.Request):
     reader = await request.multipart()
     field = await reader.next()
     filepath = f"{Var.PROJECT_PATH}/uploads/{ObjectId()}"
     # Open a new file with write-binary mode
-    
+
     with open(filepath, 'wb') as f:
         # Iterate over the field content by chunks
         while True:
@@ -29,8 +31,35 @@ async def root_route_handler(request: web.Request):
     
     return web.Response(text=resp)
 
-app = web.Application()
-app.add_routes(routes)
-
-if __name__ == "__main__":
-    web.run_app(app)
+@routes.post("/api/url")
+async def root_route_handler(request: web.Request):
+    filepath = f"{Var.PROJECT_PATH}/uploads/{ObjectId()}"
+    url = (await request.read()).decode()
+    try:
+        async with ClientSession(timeout=ClientTimeout(total=60)) as session:
+            async with session.get(url) as resp:
+                if resp.status == 200:
+                    chunk_size = 1024 * 1024
+                    data = bytearray()
+                    while len(data) < 2 * 1024 * 1024:
+                        chunk = await resp.content.read(chunk_size)
+                        if not chunk:
+                            break
+                        data.extend(chunk)
+                    
+                    # Write the data to a file
+                    with open(filepath, 'wb') as f:
+                        f.write(data)
+                    
+                    resp_text = get_metadata(filepath)
+                    os.remove(filepath)
+                else:
+                    resp_text = f"Error: Received status code {resp.status} from URL"
+    except InvalidURL as e:
+        resp_text = f"InvalidURL: {e}"
+    except TimeoutError:
+        resp_text = f"TimeOut: couldn't fetch file withon 1 minute"
+    except Exception as e:
+        resp_text = f"Error: {e}"
+    
+    return web.Response(text=resp_text)
